8e857ec4414754c2a419bceaee09d46435ac34bd
Kyle <626100+kbiondo@users.noreply.github.com>
2022-11-08 22:47:29 +0000

    followed along until 30:19 in the video

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-player_choice = "rock"
-computer_choice = "paper"
+import random
+
+
+def get_choices():
+  player_choice = input("Enter a choice (rock, paper, scissors: ")
+  options = ["rock","paper","scissors"]
+  computer_choice = random.choice(options)
+  choices = {"player":player_choice,"computer":computer_choice}
+  return choices
+
+def check_win(player, computer):
+  print(f"you chose {player}, computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
+
+check_win("rock", "paper")
