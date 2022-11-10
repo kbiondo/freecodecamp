07ae17c72d4b2cfb61b9d460b04a06d2543c1ad5
Kyle <626100+kbiondo@users.noreply.github.com>
2022-11-10 19:43:11 +0000

    finished coding to 43:43. game apparently works

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 
 def get_choices():
-  player_choice = input("Enter a choice (rock, paper, scissors: ")
+  player_choice = input("Enter a choice (rock, paper, scissors): ")
   options = ["rock","paper","scissors"]
   computer_choice = random.choice(options)
   choices = {"player":player_choice,"computer":computer_choice}
@@ -12,5 +12,26 @@ def check_win(player, computer):
   print(f"you chose {player}, computer chose {computer}")
   if player == computer:
     return "It's a tie!"
+  elif player == "rock": 
+    if computer == "paper":
+      return "Player fails"
+    else:
+      return ("player wins")
+  elif player == "scissors":
+    if computer == "paper":
+      return ("player wins!")
+    else:
+      return ("player fails")
+  elif player == "paper":
+    if computer == "rock":
+      return ("player wins!")
+    else:
+      return ("player fails")
 
-check_win("rock", "paper")
+choices = get_choices()
+result = check_win(choices["player"],choices["computer"])
+#print(choices)
+print(result)
+
+#check_win(get_choices())
+#check_win("paper", "rock")
